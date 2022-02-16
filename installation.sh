57f1597d18076e944388aed9f5ae7d4d9de7f782
Ardimas Purwita <ardimasandipurwita@outlook.com>
2022-02-17 01:09:05 +0700

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update -y
+sudo apt install caddy -y
+
+ curl -fsSL https://get.docker.com -o get-docker.sh
+ sh ./get-docker.sh
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
