dad2a0cfe2b532a64cce6331391baa88a4a50f64
Ardimas Purwita <ardimasandipurwita@outlook.com>
2022-06-20 04:35:05 +0700

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,13 +2,13 @@
 
 sudo apt update -y
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update -y
 sudo apt install caddy -y
 
- curl -fsSL https://get.docker.com -o get-docker.sh
- sh ./get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
